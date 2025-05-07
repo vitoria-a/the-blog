e2c85259fa8b37be4dd9b7e09f8a34522f43b6d5
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-05-07 11:41:28 -0300

    Create Container component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import { Container } from "@/components/Container";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
-        <h1 className="text-6xl font-bold text-center py-8">Header</h1>
+        <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
       </header>
 
       <Suspense fallback={<SpinLoader />}>
@@ -16,6 +17,6 @@ export default async function HomePage() {
       <footer>
         <p className="text-6xl font-bold text-center py-8">Footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -11,8 +11,15 @@ const JSON_POSTS_FILE_PATH = resolve(
   "seed",
   "posts.json"
 );
+const SIMULATE_WAIT_IN_MS = 0;
 
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+
+    await new Promise((resolve) => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
     const parsedJson = JSON.parse(jsonContent);
@@ -20,21 +27,20 @@ export class JsonPostRepository implements PostRepository {
     return posts;
   }
 
-  async findAllPublic(): Promise<PostModel[]> {
-    const posts = await this.readFromDisk();
-    return posts.filter((post) => post.published);
-  }
-
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
 
   async findById(id: string): Promise<PostModel> {
-    const posts = await this.findAllPublic();
+    await this.simulateWait();
+
+    const posts = await this.findAll();
     const post = posts.find((post) => post.id === id);
 
-    if (!post) throw new Error("Post não encontrado para o ID");
+    if (!post) throw new Error("Post não encontrado");
 
     return post;
   }
