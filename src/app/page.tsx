74ba5ca368c500249d2032edf368a81699c13e06
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-05-07 09:53:58 -0300

    Use Suspense to display fallback when something is loading.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { Header } from "@/components/Header";
+import { PostsList } from "@/components/PostsList";
+import { SpinLoader } from "@/components/SpinLoader";
+import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
     <div>
-      <Header />
+      <header>
+        <h1 className="text-6xl font-bold text-center py-8">Header</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p className="text-6xl font-bold text-center py-8">Footer</p>
+      </footer>
     </div>
   );
 }
