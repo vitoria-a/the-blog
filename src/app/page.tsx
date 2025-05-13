209316f05ad1625cb589131d3f22c524e2f97ba8
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-05-13 10:30:18 -0300

    Create PostImageCover component with Link + Image from Next.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
@@ -13,15 +14,18 @@ export default async function HomePage() {
       <Header />
 
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-          <Image
-            className="group-hover:scale-105 transition"
-            src="/images/bryen_0.png"
-            width={1200}
-            height={720}
-            alt="Título do post"
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: "/post/asdfasdf",
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "Alt da imagem",
+            priority: true,
+          }}
+        />
         <div className="flex flex-col gap-4 sm:justify-center">
           <time
             className="text-slate-600 block text-sm/tight"
